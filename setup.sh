eca5d60a3c09978656fa81e1d1d277046efd8a46
Amir Lavasani <amirm.lavasani@gmail.com>
2024-01-24 09:58:15 +0330

    fix: activate env in script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # change ai-service-template to your service name
-conda create --name ai-service-template python=3.11
+conda create --name ai-service-template python=3.11 && source activate ai-service-template
 
 make install-deps
 make install-dev-deps
 
 export PYTHONPATH=`pwd`
+
+make all
 
 make run
