880bda707c30afe096dee9945adbdd6d0c5becfd
Amir Lavasani <amirm.lavasani@gmail.com>
2024-02-28 11:57:31 +0330

    style: format using black

--- src/app/cli.py
+++ src/app/cli.py
@@ -13,23 +13,30 @@ def print_startup_logo():
     template_version_colored = (
         Fore.GREEN + Style.BRIGHT + "v" + __template_version__ + Style.RESET_ALL
     )
-    project_name = '''.fastapi.\nstarter'''
+    project_name = """.fastapi.\nstarter"""
     # other selected fonts
     # isometric1
     # alpha
     # smisome1
     # starwars
-    logo = Fore.CYAN + Style.BRIGHT + text2art(project_name, font="tarty1") + Style.RESET_ALL
+    logo = (
+        Fore.CYAN
+        + Style.BRIGHT
+        + text2art(project_name, font="tarty1")
+        + Style.RESET_ALL
+    )
     print(logo)
     print(
         f"\nRunning service {version_colored} 🚀 via fastapi-starter template {template_version_colored} 📦\n"
     )
+
 
 @cli.command()
 def run(host: str = "0.0.0.0", port: int = 8080):
     print_startup_logo()
 
     from app.main import run_service
+
     run_service(host, port)
 
 
